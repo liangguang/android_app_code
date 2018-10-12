e87dcd9fdc8ffac93e9030f207c4663cb4c9d5e8
Administrator <Administrator@PC-201809051031>
2018-10-12 21:02:59 +0800

    删除吧

--- ShaolinMonastery/src/com/Course/Interface/Add_LessonInterface.java
+++
@@ -1,8 +0,0 @@
-package com.Course.Interface;
-
-import java.util.List;
-import java.util.Map;
-
-public interface Add_LessonInterface {
-	 public void getData(List<Map<String, Object>> Data);//获取到课程数据
-}
